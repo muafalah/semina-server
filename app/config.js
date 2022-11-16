ce7e75f59139eee67b173ec5b4a51e2b4af78e10
muafalah <muh.ifa.amrillah@gmail.com>
2022-11-16 20:16:48 +0700

    create API Auth Signin

--- app/config.js
+++ app/config.js
@@ -3,5 +3,8 @@ dotenv.config()
 
 module.exports = {
     // Digunakan untuk menambahkan URL Database
-    urlDb: process.env.URL_MONGODB_DEV
+    urlDb: process.env.URL_MONGODB_DEV,
+    // Digunakan untuk membatasi masa expired penggunaan JWT
+    jwtExpiration: '24h',
+    jwtSecret: 'jwtSecret',
 }
---
+++ app/utils/jwt.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+const { jwtSecret, jwtExpiration } = require('../config')
+
+// Digunakan untuk membuat token JWT baru
+const createJWT = ({ payload }) => {
+    const token = jwt.sign(payload, jwtSecret, {
+        expiresIn: jwtExpiration,
+    })
+    return token
+}
+
+// Digunakan untuk mengecek apakah token benar atau salah
+const isTokenValid = ({ token }) => jwt.verify(token.jwtSecret)
+
+module.exports = {
+    createJWT,
+    isTokenValid,
+}
